33c073043885c78fc00d011baf94b628d4d38c56
Karim Barth <barth@energy-robotics.com>
2021-02-26 11:31:42 +0100

    Provide option in PointCloud to apply color mapping twice

--- src/sensors/PointCloud2.js
+++ src/sensors/PointCloud2.js
@@ -56,119 +56,130 @@ for(var i=0;i<64;i++){decode64.e[decode64.S.charAt(i)]=i;}
 *  * material (optional) - a material object or an option to construct a PointsMaterial.
 *  * colorsrc (optional) - the field to be used for coloring (default: 'rgb')
 *  * colormap (optional) - function that turns the colorsrc field value to a color
+ * * applyColorTwice (optional) - apply colormap function twice (e.g calculate min and max values to normalize)
 */
 ROS3D.PointCloud2 = function(options) {
-options = options || {};
-this.ros = options.ros;
-this.topicName = options.topic || '/points';
-this.throttle_rate = options.throttle_rate || null;
-this.compression = options.compression || 'cbor';
-this.max_pts = options.max_pts || 10000;
-this.points = new ROS3D.Points(options);
-this.lastMessages = [];
-this.decayTime = options.decay || 1;
-this.rosTopic = undefined;
-this.buffer = null;
-this.subscribe();
+  options = options || {};
+  this.ros = options.ros;
+  this.topicName = options.topic || '/points';
+  this.throttle_rate = options.throttle_rate || null;
+  this.compression = options.compression || 'cbor';
+  this.max_pts = options.max_pts || 10000;
+  this.applyColorTwice = options.applyColorTwice || false;
+  this.points = new ROS3D.Points(options);
+  this.lastMessages = [];
+  this.decayTime = options.decay || 1;
+  this.rosTopic = undefined;
+  this.buffer = null;
+  this.subscribe();
 };
 ROS3D.PointCloud2.prototype.__proto__ = THREE.Object3D.prototype;
 
 
 ROS3D.PointCloud2.prototype.unsubscribe = function(){
-if(this.rosTopic){
-  this.rosTopic.unsubscribe();
-}
+  if(this.rosTopic){
+    this.rosTopic.unsubscribe();
+  }
 };
 
 ROS3D.PointCloud2.prototype.subscribe = function(){
-this.unsubscribe();
+  this.unsubscribe();
 
-// subscribe to the topic
-this.rosTopic = new ROSLIB.Topic({
-  ros : this.ros,
-  name : this.topicName,
-  messageType : 'sensor_msgs/PointCloud2',
-  throttle_rate : this.throttle_rate,
-  queue_length : 1,
-  compression: this.compression
-});
-this.rosTopic.subscribe(this.processMessage.bind(this));
+  // subscribe to the topic
+  this.rosTopic = new ROSLIB.Topic({
+    ros : this.ros,
+    name : this.topicName,
+    messageType : 'sensor_msgs/PointCloud2',
+    throttle_rate : this.throttle_rate,
+    queue_length : 1,
+    compression: this.compression
+  });
+  this.rosTopic.subscribe(this.processMessage.bind(this));
 };
 
 ROS3D.PointCloud2.prototype.processMessage = function(msg){
-if(!this.points.setup(msg.header.frame_id, msg.point_step, msg.fields)) {
-    return;
-}
-
-var n, pointRatio = this.points.pointRatio;
-var bufSz = this.max_pts * msg.point_step;
-
-// decode message buffer
-if (msg.data.buffer) {
-  this.buffer = msg.data.slice(0, Math.min(msg.data.byteLength, bufSz));
-   n = Math.min(msg.height*msg.width / pointRatio, this.points.positions.array.length / 3);
-} else {
-  if (!this.buffer || this.buffer.byteLength < bufSz) {
-    this.buffer = new Uint8Array(bufSz);
+  if(!this.points.setup(msg.header.frame_id, msg.point_step, msg.fields)) {
+      return;
   }
-  n = decode64(msg.data, this.buffer, msg.point_step, pointRatio);
-  pointRatio = 1;
-}
 
-var dv = new DataView(this.buffer.buffer);
-var littleEndian = !msg.is_bigendian;
-var x = this.points.fields.x.offset;
-var y = this.points.fields.y.offset;
-var z = this.points.fields.z.offset;
-var base, color;
+  var n, pointRatio = this.points.pointRatio;
+  var bufSz = this.max_pts * msg.point_step;
 
-// allocate buffers to hold point data from the new message
-var newPointCount = n;
-var newPositions = new Array(newPointCount);
-var newColors = this.points.colors ? new Array(newPointCount) : undefined;
+  // decode message buffer
+  if (msg.data.buffer) {
+    this.buffer = msg.data.slice(0, Math.min(msg.data.byteLength, bufSz));
+     n = Math.min(msg.height*msg.width / pointRatio, this.points.positions.array.length / 3);
+  } else {
+    if (!this.buffer || this.buffer.byteLength < bufSz) {
+      this.buffer = new Uint8Array(bufSz);
+    }
+    n = decode64(msg.data, this.buffer, msg.point_step, pointRatio);
+    pointRatio = 1;
+  }
 
-// read data points from message and store them in the allocated buffers
-for(var i = 0; i < n; i++){
-  base = i * pointRatio * msg.point_step;
-  newPositions[3*i    ] = dv.getFloat32(base+x, littleEndian);
-  newPositions[3*i + 1] = dv.getFloat32(base+y, littleEndian);
-  newPositions[3*i + 2] = dv.getFloat32(base+z, littleEndian);
+  var dv = new DataView(this.buffer.buffer);
+  var littleEndian = !msg.is_bigendian;
+  var x = this.points.fields.x.offset;
+  var y = this.points.fields.y.offset;
+  var z = this.points.fields.z.offset;
+  var base, color;
 
-  if(this.points.colors){
+  // allocate buffers to hold point data from the new message
+  var newPointCount = n;
+  var newPositions = new Array(newPointCount);
+  var newColors = this.points.colors ? new Array(newPointCount) : undefined;
+
+  // read data points from message and store them in the allocated buffers
+  for(var i = 0; i < n; i++){
+    base = i * pointRatio * msg.point_step;
+    newPositions[3*i    ] = dv.getFloat32(base+x, littleEndian);
+    newPositions[3*i + 1] = dv.getFloat32(base+y, littleEndian);
+    newPositions[3*i + 2] = dv.getFloat32(base+z, littleEndian);
+
+    if(this.points.colors){
+        color = this.points.colormap(this.points.getColor(dv,base,littleEndian));
+        newColors[3*i    ] = color.r;
+        newColors[3*i + 1] = color.g;
+        newColors[3*i + 2] = color.b;
+    }
+  }
+
+  if(this.applyColorTwice && this.points.colors) {
+    for(var i = 0; i < n; i++){
       color = this.points.colormap(this.points.getColor(dv,base,littleEndian));
       newColors[3*i    ] = color.r;
       newColors[3*i + 1] = color.g;
       newColors[3*i + 2] = color.b;
-  }
-}
-
-// append newly read data to the message history array and discard old data
-this.lastMessages.push({ count: newPointCount, positions: newPositions, colors: newColors });
-if(this.lastMessages.length > this.decayTime){
-  this.lastMessages = this.lastMessages.splice(this.lastMessages.length - this.decayTime);
-}
-
-// write data points from message history to this.points
-var arraySize = this.points.positions.array.length;
-let addedPointCount = 0;
-// reverse loop -> start with newer data in case we have more points than can be displayed
-for(var i = this.lastMessages.length - 1; i >= 0 && addedPointCount < arraySize; i--){
-var message = this.lastMessages[i];
-for(var j = 0; j < message.count && addedPointCount < arraySize; j++){
-  this.points.positions.array[addedPointCount * 3 + 0] = message.positions[j * 3 + 0];
-  this.points.positions.array[addedPointCount * 3 + 1] = message.positions[j * 3 + 1];
-  this.points.positions.array[addedPointCount * 3 + 2] = message.positions[j * 3 + 2];
-  
-    if(this.points.colors){
-    this.points.colors.array[addedPointCount * 3 + 0] = message.colors[j * 3 + 0];
-    this.points.colors.array[addedPointCount * 3 + 1] = message.colors[j * 3 + 1];
-    this.points.colors.array[addedPointCount * 3 + 2] = message.colors[j * 3 + 2];
     }
-  
-  addedPointCount++;
-}
-}
+  }
 
-// send update message to this.points
-this.points.update(addedPointCount);
+  // append newly read data to the message history array and discard old data
+  this.lastMessages.push({ count: newPointCount, positions: newPositions, colors: newColors });
+  if(this.lastMessages.length > this.decayTime){
+    this.lastMessages = this.lastMessages.splice(this.lastMessages.length - this.decayTime);
+  }
+
+  // write data points from message history to this.points
+  var arraySize = this.points.positions.array.length;
+  let addedPointCount = 0;
+  // reverse loop -> start with newer data in case we have more points than can be displayed
+  for(var i = this.lastMessages.length - 1; i >= 0 && addedPointCount < arraySize; i--){
+    var message = this.lastMessages[i];
+    for(var j = 0; j < message.count && addedPointCount < arraySize; j++){
+      this.points.positions.array[addedPointCount * 3 + 0] = message.positions[j * 3 + 0];
+      this.points.positions.array[addedPointCount * 3 + 1] = message.positions[j * 3 + 1];
+      this.points.positions.array[addedPointCount * 3 + 2] = message.positions[j * 3 + 2];
+
+        if(this.points.colors){
+        this.points.colors.array[addedPointCount * 3 + 0] = message.colors[j * 3 + 0];
+        this.points.colors.array[addedPointCount * 3 + 1] = message.colors[j * 3 + 1];
+        this.points.colors.array[addedPointCount * 3 + 2] = message.colors[j * 3 + 2];
+        }
+
+      addedPointCount++;
+    }
+  }
+
+  // send update message to this.points
+  this.points.update(addedPointCount);
 };
