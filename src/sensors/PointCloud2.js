573a207683abe252d477957dab4c466145e68563
Martin Oehler <oehler@sim.tu-darmstadt.de>
2021-03-05 11:49:23 +0100

    Added option for decay in PointCloud2 visualization

--- src/sensors/PointCloud2.js
+++ src/sensors/PointCloud2.js
@@ -65,6 +65,8 @@ ROS3D.PointCloud2 = function(options) {
   this.compression = options.compression || 'cbor';
   this.max_pts = options.max_pts || 10000;
   this.points = new ROS3D.Points(options);
+  this.lastMessages = [];
+  this.decayTime = options.decay || 1;
   this.rosTopic = undefined;
   this.buffer = null;
   this.subscribe();
@@ -101,6 +103,7 @@ ROS3D.PointCloud2.prototype.processMessage = function(msg){
   var n, pointRatio = this.points.pointRatio;
   var bufSz = this.max_pts * msg.point_step;
 
+  // decode message buffer
   if (msg.data.buffer) {
     this.buffer = msg.data.slice(0, Math.min(msg.data.byteLength, bufSz));
      n = Math.min(msg.height*msg.width / pointRatio, this.points.positions.array.length / 3);
@@ -118,18 +121,54 @@ ROS3D.PointCloud2.prototype.processMessage = function(msg){
   var y = this.points.fields.y.offset;
   var z = this.points.fields.z.offset;
   var base, color;
+
+  // allocate buffers to hold point data from the new message
+  var newPointCount = n;
+  var newPositions = new Array(newPointCount);
+  var newColors = this.points.colors ? new Array(newPointCount) : undefined;
+
+  // read data points from message and store them in the allocated buffers
   for(var i = 0; i < n; i++){
     base = i * pointRatio * msg.point_step;
-    this.points.positions.array[3*i    ] = dv.getFloat32(base+x, littleEndian);
-    this.points.positions.array[3*i + 1] = dv.getFloat32(base+y, littleEndian);
-    this.points.positions.array[3*i + 2] = dv.getFloat32(base+z, littleEndian);
+    newPositions[3*i    ] = dv.getFloat32(base+x, littleEndian);
+    newPositions[3*i + 1] = dv.getFloat32(base+y, littleEndian);
+    newPositions[3*i + 2] = dv.getFloat32(base+z, littleEndian);
 
     if(this.points.colors){
-        color = this.points.colormap(this.points.getColor(dv,base,littleEndian));
-        this.points.colors.array[3*i    ] = color.r;
-        this.points.colors.array[3*i + 1] = color.g;
-        this.points.colors.array[3*i + 2] = color.b;
+      color = this.points.colormap(this.points.getColor(dv,base,littleEndian));
+      newColors[3*i    ] = color.r;
+      newColors[3*i + 1] = color.g;
+      newColors[3*i + 2] = color.b;
     }
   }
-  this.points.update(n);
+
+  // append newly read data to the message history array and discard old data
+  this.lastMessages.push({ count: newPointCount, positions: newPositions, colors: newColors });
+  if(this.lastMessages.length > this.decayTime){
+    this.lastMessages = this.lastMessages.splice(this.lastMessages.length - this.decayTime);
+  }
+
+  // write data points from message history to this.points
+  var arraySize = this.points.positions.array.length;
+  let addedPointCount = 0;
+  // reverse loop -> start with newer data in case we have more points than can be displayed
+  for(var i = this.lastMessages.length - 1; i >= 0 && addedPointCount < arraySize; i--){
+    var message = this.lastMessages[i];
+    for(var j = 0; j < message.count && addedPointCount < arraySize; j++){
+      this.points.positions.array[addedPointCount * 3 + 0] = message.positions[j * 3 + 0];
+      this.points.positions.array[addedPointCount * 3 + 1] = message.positions[j * 3 + 1];
+      this.points.positions.array[addedPointCount * 3 + 2] = message.positions[j * 3 + 2];
+
+      if(this.points.colors){
+        this.points.colors.array[addedPointCount * 3 + 0] = message.colors[j * 3 + 0];
+        this.points.colors.array[addedPointCount * 3 + 1] = message.colors[j * 3 + 1];
+        this.points.colors.array[addedPointCount * 3 + 2] = message.colors[j * 3 + 2];
+      }
+
+      addedPointCount++;
+    }
+  }
+
+  // send update message to this.points
+  this.points.update(addedPointCount);
 };
